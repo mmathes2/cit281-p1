05f1fab74d8cf4b7d291d803640ae5c3602cd3d4
mmathes2 <134009490+mmathes2@users.noreply.github.com>
2023-06-06 11:36:51 -0700

    added project files

---
+++ p1-date.js
@@ -0,0 +1,8 @@
+/* 
+CIT 281 Assignment 1 
+Name: Mia Matheson 
+*/
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+let d = new Date(); 
+let day = weekday[d.getDay()]; 
+console.log = day; 
---
+++ p1-random.js
@@ -0,0 +1,21 @@
+/* 
+CIT 281 Assignment 1 
+Name: Mia Matheson 
+*/
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function generateString() {
+    while(i < getRandomInteger) {
+        var result = alphabet.getRandomInteger; 
+        i++
+        return result; 
+    }
+    console.log = result; 
+}
+
